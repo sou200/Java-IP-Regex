176a2274f00764337aaefb2fff3938e00b1e0bfa
sou200 <40876805+sou200@users.noreply.github.com>
2020-04-24 23:47:43 +0000

    Update JavaRegexIP.java

--- JavaRegexIP.java
+++ JavaRegexIP.java
@@ -1,20 +1,12 @@
 import java.util.regex.*;
 
 public class JavaRegexIP {
-
 	public static void main(String[] args) {
 		String strIP = "127.0.0.255";
 		String regexIP = "\\b(2[0-5][0-5]|1[0-9][0-9]|[0-9][0-9]?)\\."
 				+ "(2[0-5][0-5]|1[0-9][0-9]|[0-9][0-9]?)\\."
 				+ "(2[0-5][0-5]|1[0-9][0-9]|[0-9][0-9]?)\\."
 				+ "(2[0-5][0-5]|1[0-9][0-9]|[0-9][0-9]?)\\b";
-		checkRegex(regex, strIP);
+		System.out.println(strIP.matches(regexIP));
 	}
-	public static void checkRegex(String regex, String str) {
-		Pattern complRegex = Pattern.compile(regex);
-		Matcher matcher = complRegex.matcher(str);
-		
-		while(matcher.find()) {
-			System.out.println(matcher.group());
-		}
-	}
+}
